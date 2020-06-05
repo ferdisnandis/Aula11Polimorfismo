f6b11ba18b2642c9fca1282a642945e69a8860c4
unknown <fe97964@gmail.com>
2020-06-05 10:01:06 -0300

    Métodos definidos

--- Calculos.cs
+++ Calculos.cs
@@ -2,6 +2,26 @@ namespace Aula11Polimorfismo
 {
     public class Calculos
     {
+        public string Calcular(){
+            return "A vida do player não precisa ser calculada";
+        }
+
+        public string Calcular(int vida){
+            return "A vida atual do player é "+vida;
+        }
         
+        public string Calcular(int vida, int escudo){
+            return "A vida do player com escudo é "+(vida+escudo);
+        }
+
+        public string Calcular(string nome, string sobrenome){
+            return $"O nome do player completo é {nome} {sobrenome}";
+        }
+
+        public string Calcular(int danoFaca, int força, int agilidade){
+            return $"Dano da máximo faca é "+((danoFaca+força+agilidade)/3);
+        }
+
+
     }
 }
